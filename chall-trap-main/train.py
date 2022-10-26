12f3547375f4a5376d66f29a33b292d42c79817b
amirleo <amirreza.mojdegan74@gmail.com>
2022-10-26 16:10:15 +0330

    first commit

---
+++ chall-trap-main/train.py
@@ -0,0 +1,13 @@
+a = input("username: ")
+b = input ("password: ")
+
+def hello(a, b):
+    user = "milad"
+    passs = "milad"
+    
+    if user == a and passs == b:
+        print("doroste")
+    else:
+        print("ghalate")
+        
+hello(a, b)
